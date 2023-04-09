5a6001052f410ba734aabf939f3baa61de210cac
Yanet Roman <yanet_0688@yahoo.com>
2023-04-09 23:38:04 +0000

    square form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+get("/square/new", {:controller => "application", :action => "blank_square_form"})
+#get("/square_root/new", {:controller => "application", :action => ""})
+#get("/payment/new", {:controller => "application", :action => ""})
+#get("/random/new", {:controller => "application", :action => ""})
 end
